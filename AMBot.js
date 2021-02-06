24d635bb421d2b8c562bc4d5ed97a30102721aba
Christopher Cosgrove <christopher.cosgrove@gmail.com>
2021-02-05 21:43:32 -0600

    Remove jQuery script append
    actual game has jquery already no need

--- AMBot.js
+++ AMBot.js
@@ -6,9 +6,9 @@
 
 var intervals = [];
 
-var amBot = document.createElement('script');
-amBot.src = "//code.jquery.com/jquery-3.2.1.min.js";
-document.getElementsByTagName('head')[0].appendChild(amBot);
+//var amBot = document.createElement('script');
+//amBot.src = "//code.jquery.com/jquery-3.2.1.min.js";
+//document.getElementsByTagName('head')[0].appendChild(amBot);
 
 var GUIsetup = setInterval(initGUI, 1000);
 
